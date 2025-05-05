6dda1ba2d8ad534614447284f3c89c1665a6f09c
faizi729 <f68857440@gmail.com>
2025-05-05 18:02:25 +0530

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Form from "./pages/Form";
  
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
             <Route path="/contact" element={<Contact />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/form/:id" element={<Form />} /> 
             {/* Add more pages as you want */}
           </Routes>
         </main>
